5624740ad7269f1a1a7481ad63d10c5224cea28f
H11TM4N <jeremiahbrownie@gmail.com>
2023-03-16 12:24:52 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this function prints a sentence followed by a new line
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
