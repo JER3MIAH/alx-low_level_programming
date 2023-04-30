da26c104560dd9c0e34023d7f0c9293114412179
H11TM4N <jeremiahbrownie@gmail.com>
2023-04-30 15:11:36 +0100

    C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a linked list
+ * @h: pointer to the first node in the linked list
+ *
+ * Return: number of elements in the freed list
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t lens = 0;
+	int diff;
+	listint_t *tempo;
+
+	if (!h || !*h)
+		return (0);
+
+	while (*h)
+	{
+		diff = *h - (*h)->next;
+		if (diff > 0)
+		{
+			tempo = (*h)->next;
+			free(*h);
+			*h = tempo;
+			lens++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			lens++;
+			break;
+		}
+	}
+
+	*h = NULL;
+
+	return (lens);
+}
+
