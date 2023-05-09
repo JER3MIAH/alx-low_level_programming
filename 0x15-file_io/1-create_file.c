249ce5df93857230552119102670d66ef28618ea
H11TM4N <jeremiahbrownie@gmail.com>
2023-05-08 21:08:06 -0700

    C - FILE I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,23 +12,17 @@
 int create_file(const char *filename, char *text_content)
 {
 	int ed, wr, lens = 0;
-
 	if (filename == NULL)
 		return (-1);
-
 	if (text_content != NULL)
 	{
 		for (lens = 0; text_content[lens];)
 			lens++;
 	}
-
 	ed = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wr = write(ed, text_content, lens);
-
 	if (ed == -1 || wr == -1)
 		return (-1);
-
 	close(ed);
-
 	return (1);
 }
