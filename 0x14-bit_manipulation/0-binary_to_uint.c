110748c19a30851bebea317d66d0f957f1bae8e2
H11TM4N <jeremiahbrownie@gmail.com>
2023-07-07 15:57:57 +0100

    First task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,21 +9,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
-	char current;
+	int i;
+	unsigned int dec_val = 0;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 
-	while ((current = *b++) != '\0')
+	for (i = 0; b[i]; i++)
 	{
-		/* Check if the current character is a valid binary digit */
-		if (current != '0' && current != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-
-		/* Convert the binary digit to its decimal value */
-		result = (result << 1) + (current - '0');
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
-	return (result);
+	return (dec_val);
 }
